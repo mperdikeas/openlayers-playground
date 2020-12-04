3f76819548d3ff67d3778f82c965f6b0b6bc1781
mperdikeas <mperdikeas@gmail.com>
2020-12-04 17:15:48 +0200

    properly typed OL 6.4.2 with webpack bundler

---
+++ 002-ts-babel-webpack/src/app.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+
+
+import {throttle} from 'lodash';
+
+
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+
+
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+ 
+
+type Props = {}
+type LocalState = {height: number | undefined, width: number | undefined}
+
+export default class App extends React.Component<Props, LocalState> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state =  {height: $(window).height(), width: $(window).width()};
+    this.updateScreenSize = throttle(this.updateScreenSize.bind(this), 1000);
+  }
+
+  private map: Map | undefined;
+
+  componentDidMount = () => {
+    this.updateScreenSize();
+    window.addEventListener('resize', this.updateScreenSize)
+
+    this.createMap();
+  }
+
+  createMap = () => {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 0
+      })
+    });
+    console.debug('map created: ', this.map);
+  }
+
+
+  updateScreenSize = () => {
+    this.setState({height: $(window).height(), width: $(window).width()});
+  }
+
+
+  render = () => {
+    return (
+      <div id='map' style={{width: this.state.width || '100%'
+                          , height: (this.state.height?`${this.state.height - 100}px`:'400px')}}>
+      </div>
+    );
+  }
+}
+
+
